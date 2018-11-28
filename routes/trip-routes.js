085425e7f8ef5a479664d8c7c017b08578a6b403
Javier <jarrindaulivi4@gmail.com>
2018-11-27 20:30:46 -0500

    get and post routes added

--- routes/trip-routes.js
+++ routes/trip-routes.js
@@ -1,9 +1,34 @@
 const express   = require('express');
 const router    = express.Router();
 const Trip      = require('../models/Trip');
+const User      = require('../models/User');
 
-router.get('/login', (req, res, next)=>{
+router.get('/trips/create', (req, res, next)=>{
   res.render('/trips/new-trip');
+});
+
+router.post('/trips/create', (req, res, next)=>{
+const theDestination = req.body.theDestination;
+const theDescription = req.body.theDescription;
+const theStartDate   = req.body.theStartDate;
+const theEndDate     = req.body.theEndDate;
+Trip.create(req.body)
+  .then(()=>{
+    res.redirect('/trip/details');
+  })
+  .catch((err)=>{
+    next(err)
+  })
+});
+
+router.get('/trips/details/:id', (req, res, next)=>{
+  Trip.findById(:id)
+  .then((theSingleTrip)=>{
+    res.render('/trips/details')
+  })
+  .catch((err)=>{
+    next(err);
+  })
 })
 
 
