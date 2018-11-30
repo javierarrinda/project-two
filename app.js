1e882154c489b51c1ccf3cff11ee986ee7c24f12
Javier <jarrindaulivi4@gmail.com>
2018-11-30 11:44:38 -0500

    updated 3000

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const passport     = require('passport');
 
 
 mongoose
-  .connect('process.env.MONGODB_URI', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
